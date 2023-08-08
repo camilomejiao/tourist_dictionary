1aeaa844414324ecc3e48cc59f14616650caf9b6
cmejia <camiloa.mejiao@gmail.com>
2023-08-08 15:56:03 -0500

    fix auth category routes

--- routes/category_routes.js
+++ routes/category_routes.js
@@ -5,7 +5,7 @@ const CategoryController =  require("../controllers/CategoryController");
 
 //Ruta Articles
 api.post("/save", [auth.auth], CategoryController.saveCategory);
-api.get("/get-categories/", [auth.auth], CategoryController.getCategories);
+api.get("/get-categories/", CategoryController.getCategories);
 api.get("/get-category/:id", [auth.auth], CategoryController.getCategory);
 api.delete("/delete-category/:id", [auth.auth], CategoryController.deleteCategory);
 api.put("/edit-category/:id", [auth.auth], CategoryController.updateCategory);
